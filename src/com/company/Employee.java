43a9dd30be43dbc62600d2026a416a98a5e19ea4
Shafirawh <widyafira@gmail.com>
2020-03-05 23:04:47 +0700

    Updated

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -3,10 +3,10 @@ package com.company;
 public class Employee {
     public int idEmployee;
     public double workHour;
-    public String []  workWeekend;
+    public int [] workWeekend;
     public String competence;
 
-    public Employee(int id_employee, double work_Hour, String [] work_Weekend, String e_competence) {
+    public Employee(int id_employee, double work_Hour, int [] work_Weekend, String e_competence) {
         idEmployee = id_employee;
         workHour = work_Hour;
         workWeekend = work_Weekend;
@@ -16,10 +16,12 @@ public class Employee {
     public int getId(){
         return idEmployee ;
     }
-    public double getWorkHour(){
+
+    public double getWorkHour() {
         return workHour;
-            }
-    public String [] getWorkWeekend() {
+    }
+
+    public int [] getWorkWeekend() {
         return workWeekend;
     }
     public String getCompetence(){
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -28,70 +28,86 @@ public class Main {
     public static void main(String[] args) throws IOException, ParseException {
 
 
-        // Read sheet1 Employee
-        String[][] read = readFile().clone();
+        // Read sheet1 Employee (Int)
+        String [][] read = readFile().clone();
         employees = new Employee[read.length];
-        for (int e = 0; e < read.length; e++) {
-            employees[e] = new Employee(
+
+        String [][] workWeekend = new String[read.length][];
+        for (int w = 0; w < read.length; w++)
+           workWeekend [w] = read[w][2].split(",");
+        int a = 0;
+        for (int i=0; i<workWeekend.length;i++){
+            if (a < workWeekend[i].length)
+                a = workWeekend[i].length;
+        }
+
+        int[][] weekend = new int[workWeekend.length][a];
+        for (int i = 0; i < workWeekend.length; i++) {
+            for (int j = 0; j < workWeekend[i].length; j++) {
+                weekend[i][j] = Integer.parseInt(workWeekend[i][j]);
+            }
+        }
+            for (int e=0; e<read.length;e++){
+                employees[e] = new Employee(
                     Integer.parseInt(read[e][0]),
                     Double.parseDouble(read[e][1]),
-                    (read[e][2].split(",")),
+                    (weekend[e]),
                     (read[e][3]));
         }
 
-             for (int i = 0; i < employees.length; i++) {
-                 System.out.println(
-                         employees[i].getId() + " " +
-                                 employees[i].getWorkHour() + " " +
-                                 employees[i].getWorkWeekend()[0] + " " +
-                                 employees[i].getWorkWeekend()[1] + " " +
-                                 employees[i].getCompetence() + " ");
-
-             }
+//        for (int i = 0; i < employees.length; i++) {
+//            System.out.println(
+//                    employees[i].getId() + " " +
+//                            employees[i].getWorkHour() + " " +
+//                            employees[i].getWorkWeekend()[0] + " " +
+//                            employees[i].getWorkWeekend()[1] + " " +
+//                            employees[i].getCompetence() + " ");
+//
+//        }
 
         //Read sheet2 Shift
         String[][] read1 = readFile1().clone();
         shifts = new Shift[read1.length];
         DateFormat time = new SimpleDateFormat("hh mm");
-            LocalTime[][] times = new LocalTime[read1.length][2];
-            for (int t = 0; t < read1.length; t++) {
-                String startTime = read1[t][10] + " " + read1[t][11];
-                String endTime = read1[t][12] + " " + read1[t][13];
-                Time start = new Time(time.parse(startTime).getTime());
-                Time end = new Time(time.parse(endTime).getTime());
-                LocalTime startShift = start.toLocalTime();
-                LocalTime endShift = end.toLocalTime();
-                times [t][0] = startShift;
-                times [t][1] = endShift;
+        LocalTime[][] times = new LocalTime[read1.length][2];
+        for (int t = 0; t < read1.length; t++) {
+            String startTime = read1[t][10] + " " + read1[t][11];
+            String endTime = read1[t][12] + " " + read1[t][13];
+            Time start = new Time(time.parse(startTime).getTime());
+            Time end = new Time(time.parse(endTime).getTime());
+            LocalTime startShift = start.toLocalTime();
+            LocalTime endShift = end.toLocalTime();
+            times [t][0] = startShift;
+            times [t][1] = endShift;
 
-            }
-//        for (int s = 0; s < read1.length; s++) {
-//            shifts[s] = new Shift(
-//                    Integer.parseInt(read1[s][0]),
-//                    Double.parseDouble(read1[s][1]), Double.parseDouble(read1[s][2]),
-//                    Double.parseDouble(read1[s][3]), Double.parseDouble(read1[s][4]),
-//                    Double.parseDouble(read1[s][5]), Double.parseDouble(read1[s][6]),
-//                    Double.parseDouble(read1[s][7]), Integer.parseInt(read1[s][8]),
-//                    (read1[s][9]), (times[s][0]), (times[s][1]), (read1[s][14]));
-//        }
-
-
-        for (int i = 0; i < shifts.length; i++) {
-           System.out.println(
-                    shifts[i].getIdShift() + " " +
-                            shifts[i].getMon() + " " +
-                            shifts[i].getTue() + " " +
-                            shifts[i].getWed() + " " +
-                            shifts[i].getThur() + " " +
-                            shifts[i].getFri() + " " +
-                            shifts[i].getSat() + " " +
-                            shifts[i].getSun() + " " +
-                            shifts[i].getShiftCat() + " " +
-                            shifts[i].getShiftName() + " " +
-                            shifts[i].getStartTime() +" "+
-                            shifts[i].getEndTime() + " " +
-                            shifts[i].getCompetence() + " ");
         }
+        for (int s = 0; s < read1.length; s++) {
+            shifts[s] = new Shift(
+                    Integer.parseInt(read1[s][0]),
+                    Double.parseDouble(read1[s][1]), Double.parseDouble(read1[s][2]),
+                    Double.parseDouble(read1[s][3]), Double.parseDouble(read1[s][4]),
+                    Double.parseDouble(read1[s][5]), Double.parseDouble(read1[s][6]),
+                    Double.parseDouble(read1[s][7]), Integer.parseInt(read1[s][8]),
+                    (read1[s][9]), (times[s][0]), (times[s][1]), (read1[s][14]));
+        }
+
+
+//        for (int i = 0; i < shifts.length; i++) {
+//            System.out.println(
+//                    shifts[i].getIdShift() + " " +
+//                            shifts[i].getMon() + " " +
+//                            shifts[i].getTue() + " " +
+//                            shifts[i].getWed() + " " +
+//                            shifts[i].getThur() + " " +
+//                            shifts[i].getFri() + " " +
+//                            shifts[i].getSat() + " " +
+//                            shifts[i].getSun() + " " +
+//                            shifts[i].getShiftCat() + " " +
+//                            shifts[i].getShiftName() + " " +
+//                            shifts[i].getStartTime() +" "+
+//                            shifts[i].getEndTime() + " " +
+//                            shifts[i].getCompetence() + " ");
+//        }
 
 
         //Read sheet3 Manpower
@@ -153,6 +169,7 @@ public class Main {
                     //Mengecek HC
                     if (checkHC2(isiEmp, i, a, j)) {
                         if (checkHC4Com(a, j)) {
+                            if (checkHC4Weekend(i,j))
                             isiEmp[j][i] = a + 1;
                             break;
                         }
@@ -160,6 +177,7 @@ public class Main {
 
 
                 }
+
             }
         }
         for (int i = 0; i < isiEmp.length; i++) {
@@ -186,7 +204,7 @@ public class Main {
                 return true;
         if (day % 7 == 1)
             if (needs(solution, shift, day) < manpowers[shift].getManTue())
-               return true;
+                return true;
         if (day % 7 == 2)
             if (needs(solution, shift, day) < manpowers[shift].getManWed())
                 //if(checkHC4Com(shift, employee))
@@ -216,6 +234,17 @@ public class Main {
         }
         return true;
     }
+    public static boolean checkHC4Weekend ( int day, int employee) {
+        if (day % 7 == 5 || day % 7 == 6 ){
+            for (int i = 0 ; i < employees[employee].getWorkWeekend().length; i ++) {
+                if (day/7==employees[employee].getWorkWeekend()[i]-1)
+                    return true;
+            }
+            return  false;
+        }
+        return true;
+    }
+
 
     @NotNull
     public static String[][] readFile() throws IOException {
@@ -241,7 +270,7 @@ public class Main {
                 mendatarfix = mendatar;
             mendatar = 0;
             menurun++;
-          //  System.out.println();
+            //  System.out.println();
         }
 
         String[][] iniarray = new String[menurun][100];
@@ -389,10 +418,6 @@ public class Main {
         return iniarray;
     }
 
- }
-
-
-
-
+}
 
 
